ed7b138af7d8c6cc9068b816355b16d50ccc91f8
Nils Hansson <nilsmenghansson@gmail.com>
2024-05-23 14:29:17 +0200

    create meme component

---
+++ my-first-react-app/src/Meme.js
@@ -0,0 +1,13 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+}
